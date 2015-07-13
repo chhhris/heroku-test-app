b0dc62af061914790f7204b633a02eccd2583c10
chhhris <mr.lake@gmail.com>
2015-07-12 23:56:41 -0700

    Correctly format responses

--- app/controllers/api/welcome_controller.rb
+++ app/controllers/api/welcome_controller.rb
@@ -3,6 +3,45 @@ module Api
 
     protect_from_forgery except: :ifttt_test_setup
 
+    DATA = [
+        {
+          "image_url": "http://example.com/images/128",
+          "title": "Banksy takes the BK",
+          "tags": "banksy, brooklyn",
+          "posted_at": "2015-11-04T09:23:00-07:00",
+          "created_at": Time.now.iso8601,
+          "id": SecureRandom.hex(10),
+          "meta": {
+            "id": SecureRandom.hex(10),
+            "timestamp": 1383597267
+          }
+        },
+        {
+          "image_url": "http://example.com/images/125",
+          "title": "Banksy takes Manhattan",
+          "tags": "banksy, nyc",
+          "posted_at": "2013-11-04T03:23:00-07:00",
+          "created_at": (Time.now - 3.hours).iso8601,
+          "id": SecureRandom.hex(8),
+          "meta": {
+            "id": SecureRandom.hex(10),
+            "timestamp": 1383580355
+          }
+        },
+        {
+          "image_url": "http://example.com/images/124",
+          "tags": "banksy, paris",
+          "title": "Banksy takes Paris, oui oui",
+          "posted_at": "2013-11-04T03:23:00-07:00",
+          "created_at": (Time.now - 1.day).iso8601,
+          "id": SecureRandom.hex(7),
+          "meta": {
+            "id": SecureRandom.hex(10),
+            "timestamp": 1383550124
+          }
+        }
+      ]
+
     # GET /welcome
     def index
       render json: { data: 'json baby'}, status: 200
@@ -20,11 +59,13 @@ module Api
       if request.headers["IFTTT-Channel-Key"] == ENV['IFTTT_Channel_Key']
         render json: {
           "data": {
-            "any_new_post": {
-              "name": "ChhhrisTest",
-              "message": "IFTTT ya heard!?"
+            "samples": {
+              "triggers":
+                {
+                  "data": DATA
+                }
             },
-          },
+          }
         }, status: 200
       else
         render json: {}, status: 401
@@ -32,41 +73,22 @@ module Api
     end
 
     def ifttt_trigger
+      data = DATA
+      if JSON.parse(request.body.read).has_key?('limit')
+        data = JSON.parse(request.body.read)['limit'].to_i == 1 ? DATA[0..0] : []
+      end
       if request.headers["IFTTT-Channel-Key"] == ENV['IFTTT_Channel_Key']
         render json: {
-          "data": [
-            {
-              "image_url": "http://example.com/images/128",
-              "title": "Banksy posty",
-              "tags": "banksy, brooklyn",
-              "posted_at": "2015-11-04T09:23:00-07:00",
-              "meta": {
-                "id": "14b9-1fd2-acaa-5df5",
-                "timestamp": 1383597267
-              }
-            },
-            {
-              "image_url": "http://example.com/images/125",
-              "tags": "banksy, nyc",
-              "posted_at": "2013-11-04T03:23:00-07:00",
-              "meta": {
-                "id": "ffb27-a63e-18e0-18ad",
-                "timestamp": 1383580355
-              }
-            },
-            {
-              "image_url": "http://example.com/images/124",
-              "tags": "banksy, paris",
-              "posted_at": "2013-11-04T03:23:00-07:00",
-              "meta": {
-                "id": "ffb27-a63e-18e0-18ad",
-                "timestamp": 1383550124
-              }
-            }
-          ]
+          "data": data
         }, status: 200
       else
-        render json: {}, status: 401
+        render json: {
+            'errors': [
+              {
+                "message": "Invalid IFTTT channel key!"
+              }
+            ]
+          }, status: 401
       end
     end
 
